e81d01308abbfe60f1a987ea7d0dc2c50472fd20
Jack Phoenix <ashley@users.mediawiki.org>
2011-08-21 22:55:03 +0000

    extensionified version of an old Wikia core hack: hide users who have made very little/no edits. SQL query by Wikia, extensionification + some tweaks by me

---
+++ FilterListUsers.i18n.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Internationalization file for the FilterListUsers extension.
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+/** English
+ * @author Jack Phoenix <jack@countervandalism.net>
+ */
+$messages['en'] = array(
+	'listusers-showall' => 'Show all users, including those with less than 5 edits',
+	'right-viewallusers' => 'View all users in [[Special:ListUsers|the user list]]'
+);
+
+/** Finnish (Suomi)
+ * @author Jack Phoenix <jack@countervandalism.net>
+ */
+$messages['fi'] = array(
+	'listusers-showall' => 'Näytä kaikki käyttäjät, mukaanlukien ne, joilla on alle viisi muokkausta',
+	'right-viewallusers' => 'Katsoa kaikki käyttäjät [[Special:ListUsers|käyttäjälistassa]]'
+);
+
+/** Dutch (Nederlands)
+ * @author Mitchel Corstjens
+ */
+$messages['nl'] = array(
+	'listusers-showall' => 'Geef alle gebruikers weer, ook gebruikers met minder dan 5 bewerkingen',
+	'right-viewallusers' => 'Bekijk alle gebruikers in de [[Special:ListUsers|gebruikers lijst]]'
+);
