103c4703482f957a1837894415c09373af3e4432
Raimond Spekking <raymond@users.mediawiki.org>
2011-08-22 19:04:31 +0000

    Localisation updates for core and extension messages from translatewiki.net

--- FilterListUsers.i18n.php
+++ FilterListUsers.i18n.php
@@ -17,12 +17,39 @@ $messages['en'] = array(
 	'right-viewallusers' => 'View all users in [[Special:ListUsers|the user list]]'
 );
 
+/** German (Deutsch)
+ * @author Kghbln
+ */
+$messages['de'] = array(
+	'filterlistusers-desc' => 'Ermöglicht das Ausblenden von Benutzern aus der [[Special:ListUsers|Benutzerliste]], die keine Bearbeitungen durchgeführt haben',
+	'listusers-showall' => 'Alle Benutzer einschließlich denjenigen anzeigen, die weniger als 5 Bearbeitungen gemacht haben',
+	'right-viewallusers' => 'Alle Benutzer in der [[Special:ListUsers|Benutzerliste]] ansehen',
+);
+
 /** Finnish (Suomi)
  * @author Jack Phoenix <jack@countervandalism.net>
  */
 $messages['fi'] = array(
 	'listusers-showall' => 'Näytä kaikki käyttäjät, mukaanlukien ne, joilla on alle viisi muokkausta',
-	'right-viewallusers' => 'Katsoa kaikki käyttäjät [[Special:ListUsers|käyttäjälistassa]]'
+	'right-viewallusers' => 'Katsoa kaikki käyttäjät [[Special:ListUsers|käyttäjälistassa]]',
+);
+
+/** Luxembourgish (Lëtzebuergesch)
+ * @author Robby
+ */
+$messages['lb'] = array(
+	'filterlistusers-desc' => 'Filtert déi Benotzer aus der [[Special:ListUsers|Benotzerlëscht]] eraus déi näischt geännert hunn',
+	'listusers-showall' => 'All Benotzer weisen, och déi mat manner wéi 5 Ännerungen',
+	'right-viewallusers' => 'All Benotzer an der [[Special:ListUsers|Benotzerlëscht]] weisen',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'filterlistusers-desc' => 'Ги филтрира корисниците што не урадувале од [[Special:ListUsers|списокот на корисници]]',
+	'listusers-showall' => 'Прикажи ги сите корисници, вклучувајќи ги оние со помалку од 5 уредувања',
+	'right-viewallusers' => 'Погледајте ги сите корисници на [[Special:ListUsers|списокот на корисници]]',
 );
 
 /** Dutch (Nederlands)
@@ -30,5 +57,6 @@ $messages['fi'] = array(
  */
 $messages['nl'] = array(
 	'listusers-showall' => 'Geef alle gebruikers weer, ook gebruikers met minder dan 5 bewerkingen',
-	'right-viewallusers' => 'Bekijk alle gebruikers in de [[Special:ListUsers|gebruikers lijst]]'
+	'right-viewallusers' => 'Bekijk alle gebruikers in de [[Special:ListUsers|gebruikers lijst]]',
 );
+
